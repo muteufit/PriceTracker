4bbfc906f351a74ee73d6fa4e3eeba929c09d9fe
Muhammed AbdElrhman <ma.prog.zero@gmail.com>
2020-01-30 00:13:28 +0200

    Creating the repo

---
+++ price_tracker.py
@@ -0,0 +1,105 @@
+from requests import get
+from bs4 import BeautifulSoup
+from time import sleep
+import smtplib
+from email.message import EmailMessage
+
+
+def sendMail():
+    msg = EmailMessage()
+    # set your email address
+    msg['From'] = 'Your Email Address'
+    # set your friend email address
+    msg['To'] = 'Your Client Email Address'
+    # set subject
+    msg['Subject'] = 'Seller'
+    # set the body
+    msg.set_content('you can now buy {0}'.format(url))
+    msg.add_alternative("""\
+    <!DOCTYPE html>
+    <html>
+        <body>
+            <h1 style="color:SlateGray;">Yay you can Buy It :) </h1>
+            <a href={0}>product</a>
+        </body>
+    </html>
+    """.format(url), subtype='html')
+    # better methode than SMTP
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        # login with your email and password 
+        smtp.login('Your Email Address', 'Your Password')
+        # send the message
+        smtp.send_message(msg)
+
+
+def getDomain():
+    domain = url.split('.')[1].split('.')[0]
+    return domain
+
+
+def makeRequest():
+    # make request
+    response = get(url)
+    # init parses
+    soup = BeautifulSoup(response.text, 'html.parser')
+    # get the right className
+    className = checkClass()
+    # make instance
+    instan = soup.find(class_=className).get_text()
+    # loop throw the result to get price
+    e=''
+    for i in instan:
+        try:
+            int(i)
+            e += i
+        except:
+            pass
+    return e
+
+
+def checkClass():
+    domainName = getDomain()
+    if domainName == 'jumia':
+        return  '-b -ltr -tal -fs24'
+    elif domainName == 'souq':
+        return 'price is sk-clr1'
+    else:
+        print('ERROR : Souq Or Jumia Only')
+        exit()
+
+    
+def jumiaPrice():
+    price=makeRequest()
+    return int(price)
+
+
+def souqPrice():
+    price=makeRequest()[:-2]
+    return int(price)
+
+
+def checkPrice():
+    domain=getDomain()
+    if domain == 'jumia':
+        return jumiaPrice()
+    else:
+        return souqPrice()
+
+
+def checkBudget():
+    # get user budget
+    budget =int(input('Your Budget (Numbers) => '))  
+    if price < budget:
+        print('========YES========')
+        sendMail()
+    else:
+        print('================')
+        print('You Need {0} LE to get this product'.format(price-budget))
+        print('================')
+        sleep(60)
+
+
+# get product url
+url = input('Product Url => ')
+price = checkPrice()
+checkBudget()
